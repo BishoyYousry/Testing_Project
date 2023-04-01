41f532696fbff32eca9dcb3fc2ed754665be1a9c
BishoyYousry <1900733@eng.asu.edu.eg>
2023-04-01 16:05:29 +0200

    Write Sample.txt, create ReadFile class and add readFile function

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+	public static void main(String args[])
+	{
+		ReadFile rf = new ReadFile();
+		String filePath = "/media/bishoy/6E90FAC190FA8F39/ASU/Senior1/2nd Term/SW Testing/Project_Invironment/src/sample.txt";
+		rf.readFile(filePath);
+	}
+}
