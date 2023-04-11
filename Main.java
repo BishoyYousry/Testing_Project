e332e961c32f597fd1dfa30315a8010bed4137c3
BishoyYousry <1900733@eng.asu.edu.eg>
2023-04-11 14:42:05 +0200

    Delete Main.java, and ReadWrite.java from Dev branch

--- Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-	public static void main(String args[])
-	{
-		ReadWrite rf = new ReadWrite();
-		String filePath = "/media/bishoy/6E90FAC190FA8F39/ASU/Senior1/2nd Term/SW Testing/Project_Invironment/src/testDataStoring.txt";
-		rf.readWrite(filePath);
-	}
-}
