23d4642e78a14a69568988d02b46421fe0503422
Bishoy Yousry <1900733@eng.asu.edu.eg>
2023-05-14 03:44:08 +0300

    Add test cases stop on test#19 in student class

--- Student_Test.java
+++ Student_Test.java
@@ -1,6 +1,8 @@
 import Exceptions.InvalidStudentIdException;
+import Exceptions.InvalidStudentMarksException;
 import Exceptions.InvalidStudentNameException;
 
+import org.junit.jupiter.api.Tag;
 import program.Student;
 
 import java.io.ByteArrayOutputStream;
@@ -24,10 +26,12 @@ class Student_Test {
 	{
 	    System.setOut(new PrintStream(outputStreamCaptor));
 	}
-	
+
+
 	@Test
+	@Tag("unit")
+	@Tag("blackbox")
 	void Test_SetName_PositiveTesting() {
-		
 		try {
 		student.setName("Bishoy Yousry");
 		}catch(InvalidStudentNameException e){
@@ -36,25 +40,44 @@ class Student_Test {
 		String expected = "Bishoy Yousry";
 		assertThat(student.getName()).isEqualTo(expected);
 	}
-	
+
+
 	@Test
+	@Tag("unit")
+	@Tag("blackbox")
 	void Test_SetName_NameStartsWithSpace_ExceptionThrown()
 	{
-		//Enter a student name that starts with space
+
 		 InvalidStudentNameException exception = assertThrows(InvalidStudentNameException.class, () -> {
 			 student.setName(" Bishoy Yousry");	// The name starts with a space
 		 });
 
 		    String expectedMessage = InvalidStudentNameException.INVALID_STUDENT_NAME_STARTS_WITH_SPACE;
 		    String actualMessage = exception.getMessage();
-        assertThat(actualMessage).contains(expectedMessage);
+        	assertThat(actualMessage).contains(expectedMessage);
 	}
-	
-	
+
 	@Test
+	@Tag("unit")
+	@Tag("blackbox")
+	void Test_SetName_NameIsEmpty_ExceptionThrown()
+	{
+
+		InvalidStudentNameException exception = assertThrows(InvalidStudentNameException.class, () -> {
+			student.setName("");
+		});
+
+		String expectedMessage = InvalidStudentNameException.INVALID_STUDENT_NAME_ALPHABETS;
+		String actualMessage = exception.getMessage();
+		assertThat(actualMessage).contains(expectedMessage);
+	}
+
+	@Test
+	@Tag("unit")
+	@Tag("blackbox")
 	void Test_SetName_NameWithoutSpaces_ExceptionThrown()
 	{
-		//Enter a student name that does not have any spaces 
+
 		 InvalidStudentNameException exception = assertThrows(InvalidStudentNameException.class, () -> {
 			 student.setName("Bishoy");	// The name does not have spaces
 		 });
@@ -66,11 +89,12 @@ class Student_Test {
 	
 	
 	@Test
+	@Tag("unit")
+	@Tag("blackbox")
 	void Test_SetName_NameHasNumbers_ExceptionThrown()
 	{
-		//Enter a student name that does not have any spaces 
 		 InvalidStudentNameException exception = assertThrows(InvalidStudentNameException.class, () -> {
-			 student.setName("Bishoy12");	// The name does not have spaces
+			 student.setName("Bishoy12");	// The name contain numbers
 		 });
 		    String expectedMessage = InvalidStudentNameException.INVALID_STUDENT_NAME_ALPHABETS;
 		    String actualMessage = exception.getMessage();
@@ -78,6 +102,8 @@ class Student_Test {
 	}
 	
 	@Test
+	@Tag("unit")
+	@Tag("blackbox")
 	void Test_SetId_PositiveTesting()
 	{				
 		String expected = "1900733s";
@@ -87,11 +113,173 @@ class Student_Test {
 	
 	
 	@Test
-	void Test_SetId_IDMustBeAlphanumeric_ExceptionThrown()
+	@Tag("unit")
+	@Tag("blackbox")
+	void Test_SetId_IDStartsWithAlphabet_ExceptionThrown()
 	{
+
 		InvalidStudentIdException exception = assertThrows(InvalidStudentIdException.class, () -> student.setId("s1900733"));
-		String expectedMessage = InvalidStudentIdException.INVALID_STUDENT_ID_DOESNOT_START_WITH_NUMBERS;
+		String expectedMessage = InvalidStudentIdException.INVALID_STUDENT_ID_DOES_NOT_START_WITH_NUMBERS;
 	    String actualMessage = exception.getMessage();
+		assertThat(actualMessage).contains(expectedMessage);
+	}
+
+
+
+
+
+	@Test
+	@Tag("unit")
+	@Tag("blackbox")
+	void Test_SetId_IDLengthLessThan8chars_ExceptionThrown()
+	{
+		InvalidStudentIdException exception = assertThrows(InvalidStudentIdException.class, () -> student.setId("1900"));
+		String expectedMessage = InvalidStudentIdException.INVALID_STUDENT_ID_INVALID_LENGTH;
+		String actualMessage = exception.getMessage();
+		assertThat(actualMessage).contains(expectedMessage);
+	}
+
+	@Test
+	@Tag("unit")
+	@Tag("blackbox")
+	void Test_SetId_IDEndsWithTwoAlphabets_ExceptionThrown()
+	{
+		InvalidStudentIdException exception = assertThrows(InvalidStudentIdException.class, () -> student.setId("123456ss"));
+		String expectedMessage = InvalidStudentIdException.INVALID_STUDENT_ID_GENERAL;
+		String actualMessage = exception.getMessage();
+		assertThat(actualMessage).contains(expectedMessage);
+	}
+
+	@Test
+	@Tag("unit")
+	@Tag("blackbox")
+	void Test_SetId_IdDoesNotContainAnyNumber_ExceptionThrown()
+	{
+		InvalidStudentIdException exception = assertThrows(InvalidStudentIdException.class, () -> student.setId("abcdefgh"));
+		String expectedMessage = InvalidStudentIdException.INVALID_STUDENT_ID_ALPHANUMERIC;
+		String actualMessage = exception.getMessage();
+		assertThat(actualMessage).contains(expectedMessage);
+	}
+
+	@Test
+	@Tag("unit")
+	@Tag("blackbox")
+	void Test_SetId_IdContainSpecialChars_ExceptionThrown()
+	{
+		InvalidStudentIdException exception = assertThrows(InvalidStudentIdException.class, () -> student.setId("123?5678"));
+		String expectedMessage = InvalidStudentIdException.INVALID_STUDENT_ID_GENERAL;
+		String actualMessage = exception.getMessage();
+		assertThat(actualMessage).contains(expectedMessage);
+	}
+
+	@Test
+	@Tag("unit")
+	@Tag("blackbox")
+	void Test_SetId_IdIsEmpty_ExceptionThrown()
+	{
+		InvalidStudentIdException exception = assertThrows(InvalidStudentIdException.class, () -> student.setId(""));
+		String expectedMessage = InvalidStudentIdException.INVALID_STUDENT_ID_INVALID_LENGTH;
+		String actualMessage = exception.getMessage();
+		assertThat(actualMessage).contains(expectedMessage);
+	}
+
+	@Test
+	@Tag("unit")
+	@Tag("blackbox")
+	@Tag("boundary")
+	void Test_setActivitiesMarkNormalValue_PositiveTesting()
+	{
+		int expected = 5;
+		try {
+			student.setActivitiesMark(5);
+		} catch (InvalidStudentMarksException e) {
+			e.printStackTrace();
+		}
+		double actual = student.getActivitiesMark();
+		assertThat(expected).isEqualTo(actual);
+	}
+
+	@Test
+	@Tag("unit")
+	@Tag("blackbox")
+	@Tag("boundary")
+	void Test_setActivitiesMarkLowerBoundValue_PositiveTesting()
+	{
+		int expected = 0;
+		try {
+			student.setActivitiesMark(0);
+		} catch (InvalidStudentMarksException e) {
+			e.printStackTrace();
+		}
+		double actual = student.getActivitiesMark();
+		assertThat(expected).isEqualTo(actual);
+	}
+
+	@Test
+	@Tag("unit")
+	@Tag("blackbox")
+	@Tag("boundary")
+	void Test_setActivitiesMarkUpperBoundValue_PositiveTesting()
+	{
+		int expected = 10;
+		try {
+			student.setActivitiesMark(10);
+		} catch (InvalidStudentMarksException e) {
+			e.printStackTrace();
+		}
+		double actual = student.getActivitiesMark();
+		assertThat(expected).isEqualTo(actual);
+	}
+
+	@Test
+	@Tag("unit")
+	@Tag("blackbox")
+	@Tag("boundary")
+	void Test_setActivitiesMarkOutLowerBoundValue_ExceptionThrown()
+	{
+		InvalidStudentMarksException exception = assertThrows(InvalidStudentMarksException.class, () ->
+				student.setActivitiesMark(-1));
+		String expectedMessage = InvalidStudentMarksException.INVALID_STUDENT_ACTIVITIES_MARK;
+		String actualMessage = exception.getMessage();
+		assertThat(actualMessage).contains(expectedMessage);
+	}
+
+	@Test
+	@Tag("unit")
+	@Tag("blackbox")
+	@Tag("boundary")
+	void Test_setActivitiesMarkOutUpperBoundValue_ExceptionThrown()
+	{
+		InvalidStudentMarksException exception = assertThrows(InvalidStudentMarksException.class, () ->
+				student.setActivitiesMark(11));
+		String expectedMessage = InvalidStudentMarksException.INVALID_STUDENT_ACTIVITIES_MARK;
+		String actualMessage = exception.getMessage();
+		assertThat(actualMessage).contains(expectedMessage);
+	}
+
+	@Test
+	@Tag("unit")
+	@Tag("blackbox")
+	void Test_setActivitiesMarkEmptyValue_ExceptionThrown()
+	{
+		Integer value = null;
+		InvalidStudentMarksException exception = assertThrows(InvalidStudentMarksException.class, () ->
+				student.setActivitiesMark(value));
+		String expectedMessage = InvalidStudentMarksException.INVALID_STUDENT_ACTIVITIES_MARK;
+		String actualMessage = exception.getMessage();
+		assertThat(actualMessage).contains(expectedMessage);
+	}
+
+	@Test
+	@Tag("unit")
+	@Tag("blackbox")
+	void Test_setActivitiesMarkFloatValue_ExceptionThrown()
+	{
+		double value = 5.5;
+		InvalidStudentMarksException exception = assertThrows(InvalidStudentMarksException.class, () ->
+				student.setActivitiesMark(value));
+		String expectedMessage = InvalidStudentMarksException.INVALID_STUDENT_ACTIVITIES_MARK;
+		String actualMessage = exception.getMessage();
 		assertThat(actualMessage).contains(expectedMessage);
 	}
 
