f8e3a4ed217471dca01268ee41ba417f6acf651d
Malek345 <73738469+Malek345@users.noreply.github.com>
2023-05-14 14:19:32 +0300

    update in eroor messages

--- Exceptions/InvalidStudentIdException.java
+++ Exceptions/InvalidStudentIdException.java
@@ -5,7 +5,7 @@ public class InvalidStudentIdException extends Exception{
 	private static final long serialVersionUID = 1L;
 	//Exception Messages]
 	public static final String INVALID_STUDENT_ID_INVALID_LENGTH = "Student id must consists of 8 chars";
-	public static final String INVALID_STUDENT_ID_ALPHANUMERIC = "Student id must contain alphanumeric chars";
+	public static final String INVALID_STUDENT_ID_ALPHANUMERIC = "Student id must contain alphanumeric char or number";
 	public static final String INVALID_STUDENT_ID_DOES_NOT_START_WITH_NUMBERS = "Student id must start with numbers";
 	public static final String INVALID_STUDENT_ID_GENERAL = "Invalid Student id";
 	
--- Exceptions/InvalidStudentMarksException.java
+++ Exceptions/InvalidStudentMarksException.java
@@ -6,9 +6,9 @@ public class InvalidStudentMarksException extends Exception {
 	//Exception Messages
 	public static final String INVALID_STUDENT_ACTIVITIES_MARK = "Student activities mark range must be between 0 to 10";
 	public static final String INVALID_STUDENT_ORAL_PRACTICAL_MARK = "Student oral practical mark range must be between 0 to 10";
-	public static final String INVALID_STUDENT_MIDTERM_MARK = "Student activities range must be between 0 to 20";
-	public static final String INVALID_STUDENT_FINAL_MARK = "Student activities range must be between 0 to 60";
-	public static final String INVALID_STUDENT_TOTAL_MARK = "Student activities range must be between 0 to 100";
+	public static final String INVALID_STUDENT_MIDTERM_MARK = "Student midterm mark range must be between 0 to 20";
+	public static final String INVALID_STUDENT_FINAL_MARK = "Student final mark range must be between 0 to 60";
+	public static final String INVALID_STUDENT_TOTAL_MARK = "Student total mark range must be between 0 to 100";
 	
 	public InvalidStudentMarksException(){
 		super();
