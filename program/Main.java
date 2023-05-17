225c6645b3b385ed9f24062391f280b94761b930
zeyadMohamed20 <zeyadmohamedasu@gmail.com>
2023-05-18 01:23:40 +0300

    Create exception message for zero space in course name

--- program/Main.java
+++ program/Main.java
@@ -1,23 +1,42 @@
-package program;
+import Exceptions.*;
+import program.*;
 
-import Exceptions.InvalidStudentMarksException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
-public class Main {
-	public static void main(String args[])
-	{
-		Student student = new Student();
-		Scanner scanner = new Scanner(System.in);
-		int value = scanner.nextInt();
-		try {
-			student.setActivitiesMark(value);
-		} catch (InvalidStudentMarksException e) {
-			e.printStackTrace();
-		} catch (InputMismatchException e)
-		{
-			System.out.println(e + "hhhhh");
-		}
-	}
+public class Main
+{
+    public static void main(String[] args)
+    {
+        FileIO f = null;
+        try
+        {
+            //f = new FileIO("D:/Testing/src/test/java/InputFile TestCases/2.txt");
+            f = new FileIO("../java/InputFile TestCases/2.txt");
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        if(f!=null)
+        {
+            try
+            {
+                f.read_file();
+            }
+            catch (InvalidStudentMarksException | InvalidCourseNameException | InvalidStudentIdException |
+                   InvalidStudentNameException | InvalidCourseCodeException | IOException e)
+            {
+                e.printStackTrace();
+            }
+            try
+            {
+                f.write_file();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
 }
